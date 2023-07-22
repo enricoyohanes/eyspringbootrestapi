e5a926d56ec3b367b8a86254d8a2ee612d63be90
Enrico Yohanes <plbenrico98@gmail.com>
2023-07-22 13:45:38 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/eyspringbootrestapi/EyspringbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.eyspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyspringbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyspringbootrestapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/juaracoding/eyspringbootrestapi/repo/XPersonRepo.java
@@ -0,0 +1,12 @@
+package com.juaracoding.eyspringbootrestapi.repo;
+
+/*IntelliJ IDEA 2023.1.2 (Community Edition)
+Build #IC-231.9011.34, built on May 16, 2023
+@Author Enrico a.k.a. Enrico Yohanes
+Java Developer
+Created on 22/07/2023 13:42
+@Last Modified 22/07/2023 13:42
+Version 1.0
+*/
+public class XPersonRepo {
+}
